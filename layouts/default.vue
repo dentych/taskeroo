45f124e74505aa0137c07cb5f7e0fddfd35569a4
dentych <dennis@tychsen.me>
2022-03-01 22:54:11 +0100

    Initial Nuxt setup

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="bg-pink-400 px-4 py-2 flex">
+      <a href="/static" class="text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+        </svg>
+      </a>
+      <a href="/profile" class="ml-auto text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+        </svg>
+      </a>
+    </div>
+    <Nuxt/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  head() {
+    return {
+      bodyAttrs: {
+        class: ["bg-gray-100", "min-h-full"]
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="px-8 mt-8 text-center" v-if="!groupID">
+      <p class="text-lg">Du er ikke i et team!</p>
+      <p class="mt-4"><a href="/group/create" class="text-violet-500">Opret ny gruppe</a>, eller få en ven til at
+        invitere
+        dig til deres gruppe.</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      name: "Hey",
+      groupID: null,
+    }
+  }
+})
+</script>
